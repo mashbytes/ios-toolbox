7664f4761af8cb707b8168e9bd042a6f6e5aa196
Matthew Hobbs <mh@youtility.co.uk>
2022-04-12 09:19:28 +0100

    allow uiapplication delegates to return false to openURL

--- IOSToolbox/IOSToolbox/Bootstrap/Array+UIApplicationDelegate.swift
+++ IOSToolbox/IOSToolbox/Bootstrap/Array+UIApplicationDelegate.swift
@@ -2,38 +2,38 @@ import Foundation
 import UIKit
 
 extension Array where Element: UIApplicationDelegate {
-    
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool  {
         return reduce(true) { acc, delegate in
             return acc && (delegate.application?(application, didFinishLaunchingWithOptions: launchOptions) ?? true)
         }
     }
-    
+
     func applicationDidBecomeActive(_ application: UIApplication) {
         forEach { delegate in
             delegate.applicationDidBecomeActive?(application)
         }
     }
-    
+
     func applicationWillResignActive(_ application: UIApplication) {
         forEach { delegate in
             delegate.applicationWillResignActive?(application)
         }
     }
-    
+
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        return reduce(false) { acc, delegate in
-            let opened = delegate.application?(app, open: url, options: options) ?? false
-            return acc || opened
+        return reduce(true) { acc, delegate in
+            let success = (delegate.application?(app, open: url, options: options) ?? false)
+            return acc || success
         }
     }
-    
+
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         forEach { delegate in
             delegate.application?(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
         }
     }
-    
+
     func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
         forEach { delegate in
             delegate.application?(application, didFailToRegisterForRemoteNotificationsWithError: error)
@@ -45,7 +45,7 @@ extension Array where Element: UIApplicationDelegate {
             $0.applicationDidEnterBackground?(application)
         }
     }
-    
+
     func applicationWillEnterForeground(_ application: UIApplication) {
         forEach {
             $0.applicationWillEnterForeground?(application)
